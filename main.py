4609d8b1b85ba98d988d68386277f28b9f614fd5
Crysthian Zanote Armini <52865098+crysthianzaar@users.noreply.github.com>
2023-11-23 00:35:14 -0300

    include SocketIO

--- main.py
+++ main.py
@@ -1,18 +1,43 @@
 from flask import Flask, render_template
+from flask_socketio import SocketIO
 import serial
+import threading
 
 app = Flask(__name__)
+socketio = SocketIO(app, reconnection=True, reconnection_attempts=3)
+
+
+ser_lock = threading.Lock()
+ser_instance = None
+
+
+def initialize_serial():
+    global ser_instance
+    with ser_lock:
+        if ser_instance is None:
+            ser_instance = serial.Serial('COM5', 9600)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/update_data')
-def update_data():
-    # Abre a porta serial e lê o valor
-    with serial.Serial('COM5', 9600) as ser:
-        data = ser.readline().decode('utf-8').strip()
-        return {'value': data}
+
+@socketio.on('update_request')
+def handle_update_request():
+    initialize_serial()  # Garante que a porta serial está inicializada
+    with ser_lock:
+        if ser_instance is not None:
+            try:
+                data = ser_instance.readline().decode('utf-8').strip()
+                if data:
+                    print('valor enviado via Socket.IO:', data)
+                    socketio.emit('update_data', {'value': data})
+            except Exception as e:
+                print('Erro na leitura serial:', e)
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+
+    # Inicia o servidor Flask-SocketIO
+    socketio.run(app, debug=True)
